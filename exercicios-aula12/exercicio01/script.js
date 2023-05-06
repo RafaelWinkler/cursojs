5341b07164c7765d3143de329d32234c05f390f2
RafaelWinkler <rafaeldiaswinkler41@gmail.com>
2023-05-06 07:55:23 -0300

    trabalhando condicionais

--- exercicios-aula12/exercicio01/script.js
+++ exercicios-aula12/exercicio01/script.js
@@ -1,17 +1,21 @@
 function carregar() {
     var msg = window.document.getElementById('msg')
-    var imagem = window.document.getElementById('imagem')
+    var imagem = window.document.getElementById('img')
     var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
+    //var hora = data.getHours()
+    var hora = '14'
+        msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         //Bom Dia
-        imagem.src = '../imagens/manha-p.png'
-    } else if (hora >= 12 && hora < 18) {
+        imagem.src = 'imagens/manha.png'
+        document.body.style.background = '#f6e38c'
+    } else if (hora >= 12 && hora <= 18) {
         //Boa Tarde
-        imagem.src = '../imagens/tarde-p.png'
+        imagem.src = 'imagens/tarde.png'
+        document.body.style.background = '#c82601'
     } else {
         //Boa Noite
-        imagem.src = '../imagens/noite-p.png'
+        imagem.src = 'imagens/noite.png'
+        document.body.style.background = '#141821'
     }
 }
