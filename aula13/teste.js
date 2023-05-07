38e84f6fe33b2b4584f7b5b17e344e6952531561
RafaelWinkler <rafaeldiaswinkler41@gmail.com>
2023-05-07 12:47:04 -0300

    trabalhando com repetições

---
+++ aula13/teste.js
@@ -0,0 +1,23 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`);
+    c++
+} while(c <= 6)
+
+
+
+
+
+/*while (c <= 6) {
+    console.log(`Passo ${c}`);
+    c++
+} Teste lógico no início, essa também é a mais encontrada nos programas*/ 
+
+
+
+/*console.log('Olá, yudo bem?');
+console.log('Olá, yudo bem?');
+console.log('Olá, yudo bem?');
+console.log('Olá, yudo bem?');
+console.log('Olá, yudo bem?');
+console.log('Olá, yudo bem?');*/
