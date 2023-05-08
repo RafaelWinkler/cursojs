7b4c8a207c39952dc12f9c25d99a0439b0b76a92
RafaelWinkler <rafaeldiaswinkler41@gmail.com>
2023-05-08 16:44:19 -0300

    exercício 17 realizado

---
+++ aula14-exercicios/ex017/script.js
@@ -0,0 +1,18 @@
+function Gerar() {
+    let num = document.querySelector('#inum')
+    let res = document.querySelector('#ires')
+    if (num.value.length == '') {
+        alert('[ERRO] É necessário digitar um número para gerar a tabuada!')
+    } else {
+        let c = 1
+        let n = Number(num.value)
+        let tab = 0
+        res.innerHTML = ''
+        for (c = 1 ; c <= 10; c++) {
+            let tab = n * c
+            let item = document.createElement('option')
+            item.text = `${n} X ${c} = ${tab}`
+            res.appendChild(item)
+        }
+    }
+}
